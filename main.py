7e52cb87be0900a445d553cf1b83920992c3ca3b
Twid18 <81539854+Twid18@users.noreply.github.com>
2024-07-15 16:09:48 -0700

    Created working branch

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pandas import DataFrame
 class CSV:
     # class variable since it is used only w/in class
     CSV_FILE = 'finance_data.csv'
-
+    COLUMNS = ['date', 'amount', 'category', 'description']
     # decorator to give access to class itself - not instance
     @classmethod
     # initialize the csv file
@@ -18,9 +18,21 @@ class CSV:
         try:
             pd.read_csv(cls.CSV_FILE)
         except FileNotFoundError:
-            df: DataFrame = pd.DataFrame(columns=['date', 'amount', 'category', 'description'])
+            df = pd.DataFrame(cls.COLUMNS)
             df.to_csv(cls.CSV_FILE, index=False)
 
-
 # Test:
-CSV.initialize_csv()
+# CSV.initialize_csv()
+
+    @classmethod
+    def add_entry(cls, date, amount, category, description):
+        # Using a dictionary to write into columns
+        new_entry = {
+            'date': date,
+            'amount': amount,
+            'category': category,
+            'description': description
+        }
+        with open(cls.CSV_FILE, 'a', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
