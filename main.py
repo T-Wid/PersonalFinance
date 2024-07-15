ebe35ee7c0b5df8cab89f793e653195c3121b905
Twid18 <81539854+Twid18@users.noreply.github.com>
2024-07-15 14:15:00 -0700

    hello

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-
+f
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 def hello() -> object:
     print('hello')
