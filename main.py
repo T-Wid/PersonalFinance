7f649b7845aa011be4859b0c1b323a7e0b3821e4
Twid18 <81539854+Twid18@users.noreply.github.com>
2024-07-17 13:44:20 -0700

    Created get_transactions, which utilizes pandas dataframe filtering, .loc indexing, and formatting to find transactions from the specified timeframe as well as total income, total expenses, and net earnings

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib
 import csv
 from datetime import datetime
-from data_entry import get_date, get_amount, get_description, get_category
+from data_entry import get_date, get_amount, get_description, get_category, date_format
 
 # set up a class with multiple methods to make working with CSV file easy
 from pandas import DataFrame
@@ -12,6 +12,7 @@ class CSV:
     # class variable since it is used only w/in class
     CSV_FILE = 'finance_data.csv'
     COLUMNS = ['date', 'amount', 'category', 'description']
+
     # decorator to give access to class itself - not instance
     @classmethod
     # initialize the csv file
@@ -22,8 +23,8 @@ class CSV:
             df = pd.DataFrame(cls.COLUMNS)
             df.to_csv(cls.CSV_FILE, index=False)
 
-# Test:
-# CSV.initialize_csv()
+    # Test:
+    # CSV.initialize_csv()
 
     @classmethod
     def add_entry(cls, date, amount, category, description):
@@ -39,5 +40,43 @@ class CSV:
             writer.writerow(new_entry)
         print("Entry added successfully.")
 
-CSV.initialize_csv()
-CSV.add_entry(c)
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df['date'] = pd.to_datetime(df['date'], format=CSV.date_format)
+        start_date = datetime.strptime(start_date, CSV.date_format)
+        end_date = datetime.strptime(end_date, CSV.date_format)
+
+        # bitwise &, needed when using pandas df and mask rather than 'AND'
+        mask = (df['date'] >= start_date & (df['date'] <= end_date))
+
+        # using loc to find matches with the criteria of mask
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print('No transactions were found in the given timeframe.')
+        else:
+            print(f'Transactions from {start_date.strftime(CSV.date_format)}) to {end_date.strftime(CSV.date_format)}')
+            print(filtered_df.to_string(index=False, formatter={'date': lambda x: x.strftime(CSV.date_format)}))
+            total_income = filtered_df[filtered_df['category'] == 'Income']['amount'].sum()
+            total_expense = filtered_df[filtered_df['category'] == 'Expense']['amount'].sum()
+            print('\nSummary')
+            print(f'Total Income: ${total_income:.2f}')
+            print(f'Total Expense: ${total_expense:.2f}')
+            print(f'Net Earnings: ${(total_income - total_expense):.2f}')
+
+        return filtered_df
+
+
+
+def add():
+    CSV.initialize_csv()
+    date = get_date('Enter the date of the transaction (dd-mm-YY), program will default to current date:',
+                    allow_default=True)
+    amount = get_amount()
+    category = get_category()
+    description = get_description()
+    CSV.add_entry(date, amount, category, description)
+
+
+add()
